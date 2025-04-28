e7037a1f079149716409b9c13252a34e958e2c4d
asrulabdullah99 <srulabdullah@gmail.com>
2025-04-29 06:40:48 +0700

    update customer controller

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use Pest\ArchPresets\Custom;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class CustomerController extends Controller
 {
@@ -15,4 +15,77 @@ class CustomerController extends Controller
         $dataUCustomer = Customer::all();
         return response()->json($dataUCustomer, 200);
     }
+
+    public function show($id): JsonResponse
+    {
+        try {
+            $customer = Customer::findOrFail($id);
+            return response()->json($customer, 200);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['message' => 'Customer tidak ditemukan'], 404);
+        }
+    }
+
+    // Menambahkan user baru
+    public function store(Request $request): JsonResponse
+    {
+        $request->validate([
+            'customer_name' => 'required|string|max:255',
+            'alamat' => 'required|string|max:255',
+            'no_hp' => 'required|max:15|min:8|unique:customer,no_hp',
+        ]);
+
+        $customer = Customer::create([
+            'customer_name' => $request->customer_name,
+            'alamat' => $request->alamat,
+            'no_hp' => $request->no_hp,
+        ]);
+
+
+        return response()->json([
+            'message' => 'Akun customer berhasil ditambahkan.',
+            'data' => $customer
+        ], 201);
+    }
+
+      // Mengupdate data user
+      public function update(Request $request, $id): JsonResponse
+      {
+          try {
+              $customer = Customer::findOrFail($id);
+  
+              $request->validate([
+                'customer_name' => 'sometimes|required|string|max:255',
+                'alamat' => 'sometimes|required|string|max:255',
+                'no_hp' => 'sometimes|required|max:15|min:8|unique:customer,no_hp',
+            ]);
+  
+              // Hanya update field yang dikirim
+              $data = $request->only(['customer_name', 'alamat', 'no_hp']);
+
+              $customer->update($data);
+              
+  
+              return response()->json([
+                  'message' => $customer->wasChanged()
+                      ? 'Akun pengguna berhasil diupdate.'
+                      : 'Tidak ada perubahan pada data pengguna.',
+                  'data' => $customer
+              ], 200);
+          } catch (ModelNotFoundException $e) {
+              return response()->json(['message' => 'Akun tidak ditemukan'], 404);
+          }
+      }
+  
+      public function destroy($id): JsonResponse
+      {
+          try {
+              $customer = Customer::findOrFail($id);
+              $customer->delete();
+  
+              return response()->json(['message' => 'Customer berhasil dihapus.']);
+          } catch (ModelNotFoundException $e) {
+              return response()->json(['message' => 'Customer tidak ditemukan.'], 404);
+          }
+      }
 }
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,19 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Customer extends Model
 {
-    use HasApiTokens, HasFactory, HasUlids;
-
+    use HasUlids;
+    
+    protected $table = 'customer';
+    
     protected $fillable = [
         'customer_name',
         'alamat',
         'no_hp',
-
     ];
+
+    protected function casts(): array
+    {
+        return [
+            'customer_name' => 'string',
+            'no_hp' => 'string',
+        ];
+    }
+
 }
